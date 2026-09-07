7c344513f8b1606c84e7f7ba61cc663d360715b6
Sumaiya Taher <110553442+sumaiyataher@users.noreply.github.com>
2026-09-07 10:57:57 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function (params) {
+  $('.js-preloader').preloadinator({
+   scroll: false,
+  minTime: 300,
+  animation: 'fadeOut',
+  animationDuration: 400,
+  
+  });
+  $('.slick').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+  responsive: [
+
+
+    {
+      breakpoint: 991,
+      settings: {
+        autoplay: true,
+        arrows:false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        autoplay: true,
+        arrows:false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        autoplay: true,
+        arrows:false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+});
+
+// Aos
+AOS.init({
+	offset: 100,
+	delay: 50,
+	duration: 1000,
+	easing: 'linear'
+
+});
+
+    
+
+
+  
+});
+
